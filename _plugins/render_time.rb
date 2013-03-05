779006b9409269fd83a217e7d1cacea0c6d25cb1
McCabe <mccabe@tacplan.com>
2013-03-05 15:36:28 -0500

    Revert "time"
    This reverts commit cf1c780cae752f673aefb968c5e7c321e55f4454.

--- _plugins/render_time.rb
+++ _plugins/render_time.rb
@@ -1,12 +1,7 @@
-# Title: RenderTimeTag
-# Source: https://github.com/mojombo/jekyll/wiki/Plugins
-# Description: Sample `render_time` tag
-
-require 'yaml'
-
 module Jekyll
+  module Tags
+    class RenderTimeTag < Liquid::Tag
 
-  class RenderTimeTag < Liquid::Tag
     def initialize(tag_name, text, tokens)
       super
       @text = text
@@ -16,14 +11,6 @@ module Jekyll
       "#{@text} #{Time.now}"
     end
   end
-
 end
 
-config = YAML.load_file(File.join(File.dirname(__FILE__), '..', '..', 'source', '_config.yml'))
-if !config.key?("disabled_tags")
-  Liquid::Template.register_tag('render_time', Jekyll::RenderTimeTag)
-else
-  disabled = config["disabled_tags"]
-  disabled = [disabled] if disabled.is_a?('String')
-  Liquid::Template.register_tag('render_time', Jekyll::RenderTimeTag) unless disabled.member?('render_time')
-end
+Liquid::Template.register_tag('render_time', Jekyll::Tags::RenderTimeTag)
