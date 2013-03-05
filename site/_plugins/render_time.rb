cf1c780cae752f673aefb968c5e7c321e55f4454
McCabe <mccabe@tacplan.com>
2013-03-05 15:27:29 -0500

    time

--- site/_plugins/render_time.rb
+++ site/_plugins/render_time.rb
@@ -1,7 +1,12 @@
-module Jekyll
-  module Tags
-    class RenderTimeTag < Liquid::Tag
+# Title: RenderTimeTag
+# Source: https://github.com/mojombo/jekyll/wiki/Plugins
+# Description: Sample `render_time` tag
 
+require 'yaml'
+
+module Jekyll
+
+  class RenderTimeTag < Liquid::Tag
     def initialize(tag_name, text, tokens)
       super
       @text = text
@@ -11,6 +16,14 @@ module Jekyll
       "#{@text} #{Time.now}"
     end
   end
+
 end
 
-Liquid::Template.register_tag('render_time', Jekyll::Tags::RenderTimeTag)
+config = YAML.load_file(File.join(File.dirname(__FILE__), '..', '..', 'source', '_config.yml'))
+if !config.key?("disabled_tags")
+  Liquid::Template.register_tag('render_time', Jekyll::RenderTimeTag)
+else
+  disabled = config["disabled_tags"]
+  disabled = [disabled] if disabled.is_a?('String')
+  Liquid::Template.register_tag('render_time', Jekyll::RenderTimeTag) unless disabled.member?('render_time')
+end
