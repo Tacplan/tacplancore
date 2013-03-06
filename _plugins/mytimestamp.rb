9bc711c623443da0dc6558229f6a28ce3098ac96
McCabe <mccabe@tacplan.com>
2013-03-06 18:57:48 -0500

    date

--- _plugins/mytimestamp.rb
+++ _plugins/mytimestamp.rb
@@ -1,9 +1,9 @@
 module Jekyll
-  module mytimestamp
+  module MyTimeStamp
      def mydatetime(date)
             date.strftime("%D %B %Y %H%M")
      end
   end
 end
 
-Liquid::Template.register_filter(Jekyll::mytimestamp)
+Liquid::Template.register_filter(Jekyll::MyTimeStamp)
